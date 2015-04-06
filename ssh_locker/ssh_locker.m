2e79ca4a7b754b72e427a4855f20acef3c1a9a33
Jorge <chocolate.camera@gmail.com>
2015-04-06 12:20:57 +0200

    Add keychain locking when screen saver activates.
    From https://code.google.com/p/ddribin/issues/detail?id=3
    
    > Reported by jszakmei...@gmail.com, Jun 28, 2011
    >
    > Thank you Dave for providing ssh_locker, and a sound way to kill the
    > ssh-agent on sleep/hibernate! I've been making use of this tool for
    > years now.  I finally figured it was time to make it lock when the
    > screen saver kicks in. Here is a patch that does the trick. I'm not
    > an Objective-C person, but I did test it, and it does appear to
    > work.

--- ssh_locker/ssh_locker.m
+++ ssh_locker/ssh_locker.m
@@ -1,5 +1,44 @@
 #import <Foundation/Foundation.h>
 
+@interface LockAgent : NSObject {
+}
+
+- (id)init;
+- (void)dealloc;
+- (void)lock_keychain:(NSNotification *)note;
+@end
+
+@implementation LockAgent
+
+- (id)init
+{
+    if (![super init])
+        return nil;
+
+    NSDistributedNotificationCenter *center =
+        [NSDistributedNotificationCenter defaultCenter];
+    [center addObserver:self
+               selector:@selector(lock_keychain:)
+                   name:@"com.apple.screensaver.didstart"
+                 object:nil];
+
+    return self;
+}
+
+- (void)dealloc
+{
+    NSDistributedNotificationCenter *center =
+        [NSDistributedNotificationCenter defaultCenter];
+    [center removeObserver:self];
+    [super dealloc];
+}
+
+- (void)lock_keychain:(NSNotification *)note
+{
+    SecKeychainLockAll();
+}
+
+@end
 
 int stop_agent(void)
 {
@@ -26,8 +65,9 @@ OSStatus keychain_locked(SecKeychainEvent keychainEvent, SecKeychainCallbackInfo
 int main (int argc, const char * argv[])
 {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    LockAgent *agent = [[LockAgent alloc] init];
 
-	SecKeychainAddCallback(&keychain_locked, kSecLockEventMask, nil);
+    SecKeychainAddCallback(&keychain_locked, kSecLockEventMask, nil);
 
     [[NSRunLoop currentRunLoop] run];
 
